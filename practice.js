9d54412fe427c92542921b61b9378fc59b1db973
davissekai <davisdelali24@gmail.com>
2024-05-08 09:59:26 +0100

    new js stuff

---
+++ practice.js
@@ -0,0 +1,35 @@
+/* a for loop
+
+let count; 
+
+count = 10;
+
+for (i = 0; i < 8; i = i + 1) {
+    console.log(i)
+} */
+
+
+const character = "!";
+const count = 8;
+const rows = [];
+let inverted = true;
+
+function padRow(rowNumber, rowCount) {
+  return " ".repeat(rowCount - rowNumber) + character.repeat(2 * rowNumber - 1) + " ".repeat(rowCount - rowNumber);
+}
+
+for (let i = 1; i <= count; i++) {
+  if (inverted) {
+    rows.unshift(padRow(i, count));
+  } else {
+    rows.push(padRow(i, count));
+  }
+}
+
+let result = ""
+
+for (const row of rows) {
+  result = result + "\n" + row;
+}
+
+console.log(result);
